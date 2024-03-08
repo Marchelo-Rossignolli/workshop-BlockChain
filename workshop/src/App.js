e30dc9170c213ba9e3c463566bb5bf821e5821ea
Marcelo Rossignolli <marcelo.rossignolli@sou.inteli.edu.br>
2024-03-08 08:09:50 -0300

    daf

---
+++ workshop/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="border-2 border-red-500">
+      <h1 className="text-bold text-white"> Helo, world!</h1>
+      <p> Olá Inteli</p>
+    </div>
+  );
+}
+
+export default App;
