a902bcdde78bd4ce0d5cce66dc0d8381d9048e00
Marcelo Rossignolli <marcelo.rossignolli@sou.inteli.edu.br>
2024-03-13 17:59:39 -0300

    13/03

--- workshop/src/App.js
+++ workshop/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import Input from './components/input';
+import Button from './components/Button';
 function App() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-        <div className="bg-red-600 w-5/6">
-          <p>
-            Receba sua Proof-Of_Attandance NFT
+        <div className="bg-black w-1/3">
+          <p className="text-white">
+            Receba sua Proof-Of-Attandance NFT
           </p>
 
-          <p className="">
-            Para receber sua NFT você precisa ter participado do evento e ter uma carteira na rede Sepolia
+          <p className="text-white">
+            Para receber sua NFT você precisa ter participado do evento e possuir uma carteira na rede Sepolia
           </p>
 
-          <form className="flex flex-col mt-8">
+          <form className="flex flex-col mt-8 justify-center items-center">
             <Input 
               description="Nome de Usuário"
-              label="insira seu nome" />
+              label="insira seu nome" 
+              placeholder="Marcelo"
+              type="text"
+            />
+            <Input 
+              description="Email para receber novidades"
+              label="Email" 
+              placeholder="mareclorrossignolli2005@gmail.com"
+              type="email"
+            />
+
+            <Button type="submit" name="Gerar NFT" ></Button>
+            <Button type="submit" name="Marcelo" ></Button>
+
 
           </form>
         </div>
--- workshop/src/components/Button.js
+++ workshop/src/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props){
     return(
-        <button className="text-xl mt-4 p-2 bg-blue-500"
+        <button className="text-xl mt-4 p-2 bg-blue-500 w-1/2"
             type={props.type}
             >
             {props.name}
